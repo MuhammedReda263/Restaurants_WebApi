0399cd0da33136641da520aee5dff2352390ced0
Muhammed Reda <moreda263@gmail.com>
2025-08-12 09:28:36 +0300

    Setting up infrastructure

--- Restaurants.Domin/Entities/Dish.cs
+++ Restaurants.Domin/Entities/Dish.cs
@@ -8,6 +8,8 @@ namespace Restaurants.Domin.Entities
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public decimal Price { get; set; }
+        public int? KiloCalories { get; set; }
+        public int RestauratId { get;  set; }
 
     }
 }
